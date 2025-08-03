927fe2b2f48a033c66da02c6afda896f18578c2a
Germain <germain@MacBook-Pro-de-Germain.local>
2025-08-03 19:24:54 +0200

    Ajout de fixtures : user Admin

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Création du user Admin
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setRoles(['ROLE_ADMIN']);
+        // Hash du mot de passe
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'adminpassword'
+        );
+        $admin->setPassword($hashedPassword);
+        $manager->persist($admin);
+
+        // Création de 3 tâches pour l'Admin
+        for ($i = 1; $i <= 3; $i++) {
+            $task = new Task();
+            $task->setTitle(sprintf('Tâche %d', $i));
+            $task->setContent(sprintf('Contenu de la tâche %d', $i));
+            $task->setAuthor($admin);
+            $manager->persist($task);
+        }
+        $manager->flush();
+    }
+}
